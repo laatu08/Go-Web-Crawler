4aeb3f1f4723e9832faba371b02f2bc6b6052ee6
laatu08 <partha2002borah@gmail.com>
2026-01-08 23:17:13 +0530

    config added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module concurrent-web-crawler
+
+go 1.24.4
---
+++ internal/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import "time"
+
+type Config struct{
+	SeedURL 	string 		// starting url
+	Workers		int 		// number of concurrent worker
+	MaxDepth 	int 		// how deep to crawl
+	MaxPages 	int 		// safety limit
+	Timeout		time.Duration	// HTTP timeout
+	SameDomain	bool 		// crawl only same domain
+}
+
+// DefaultConfig returns defaults
+func DefaultConfig(seed string) Config {
+	return Config{
+		SeedURL:    seed,
+		Workers:    5,
+		MaxDepth:   2,
+		MaxPages:   100,
+		Timeout:    10 * time.Second,
+		SameDomain: true,
+	}
+}
